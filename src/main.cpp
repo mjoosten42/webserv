a3ea4a1845a91d73edab6402f527f73bd19cec1f
Maarten Joosten <mjoosten@student.codam.nl>
2022-10-03 15:35:29 +0200

    amogus

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include "utils.hpp"
+
+#define BUFSIZE 1024
 
 int	main() {
 	sockaddr_in	addr;
+	sockaddr	client;
+	socklen_t	tmp = 1;
+	char		buf[BUFSIZE];
+
 	int 		fd;
-	int i = 1;
+	int			conn;
 	
 	fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -14,16 +21,33 @@ int	main() {
 	addr.sin_port = htons(8080);
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	print(addr.sin_port);
 
+	std::cout << "Now listening on "  << ntohs(addr.sin_port) << std::endl;
 
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &i, sizeof(i)) < 0)
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &tmp, sizeof(tmp)) < 0)
 		perror("setsockopt");
 	
 	if (bind(fd, (sockaddr *)&addr, sizeof(addr)) < 0)
 		perror("bind");
 
-	if (listen(fd, 3) < 0)
+	if (listen(fd, 1) < 0)
 		perror("listen");
+
+	while (1) {
+		
+		conn = accept(fd, &client, &tmp);
+		if (conn < 0)
+			perror("accept");
+
+		while (1) {
+			bzero(buf, BUFSIZE - 1);
+			recv(conn, buf, BUFSIZE - 1, 0);
+			print(buf);
+			std::string	str("HTTP/1.1 200 OK\nCache-Control: no-cache\nServer: libnhttpd\nDate: Wed Jul 4 15:32:03 2012\nConnection: Keep-Alive:\nContent-Type: text/plain\nContent-Length: 6\n\namogus");
+			send(conn, str.c_str(), str.length(), 0);
+			print(buf);
+		}
+	}
+
+	close(fd);
 }
