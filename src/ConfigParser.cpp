3f732d98d28885a7b16db9e54e92dfbc7d90c6b5
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-10-10 18:03:09 +0200

    Working on finite state machine for parsing config files.

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -6,13 +6,19 @@
 
 ConfigParser::ConfigParser() {
 	m_main_context.name = "main";
+	m_tokens[SPACE] = ' ';
+	m_tokens[SEMICOLON] = ';';
+	m_tokens[COMMENT] = '#';
+	m_tokens[OPEN_BRACE] = '{';
+	m_tokens[CLOSE_BRACE] = '}';
 }
 
 bool ConfigParser::parse_config(const char *path) {
 	std::vector<std::string> config_file = loadConfigToStrVector(path);
 	discardComments(config_file);
+	finite_state_machine(config_file);
 	//  std::vector<std::string>::iterator it;
-	//  for (it = conf_vector.begin(); it != conf_vector.end(); ++it) {
+	//  for (it = config_file.begin(); it != config_file.end(); ++it) {
 	//          print(*it);
 	//  }
 
@@ -44,15 +50,49 @@ std::vector<std::string> ConfigParser::loadConfigToStrVector(const char *path) {
 void ConfigParser::discardComments(std::vector<std::string>& config) {
 	std::vector<std::string>::iterator it;
 	for (it = config.begin(); it != config.end(); ++it) {
-		size_t pos = (*it).find_first_of('#');
+		size_t pos = (*it).find_first_of(m_tokens[COMMENT]);
 		if (pos != std::string::npos)
 			(*it).resize(pos);
 	}
 }
 
-// STORING TO PARSING STRUCT
+// FINITE STATE MACHINE:
 
-//  Tokens to consider: ' ' ';' '#' '{' '}'
+void ConfigParser::finite_state_machine(std::vector<std::string>& file)
+{
+	std::vector<std::string>::iterator it;
+	for (it = file.begin(); it != file.end(); ++it) {
+		size_t pos = (*it).find_first_of(m_tokens + SEMICOLON, 0, SIZE); // Find first token in line that isn't a space
+		if (pos != std::string::npos){
+			// print((*it)[pos]);
+			// switch ((*it)[pos]):
+			// {
+			// 	case m_tokens[SEMICOLON]:
+			// 	case m_tokens[OPEN_BRACE]:
+			// 	case m_tokens[CLOSE_BRACE]:
+			// 	default:
+			// 		continue //Pointer to current state and run that function?
+			// }
+		}
+	}
+}
+
+void ConfigParser::state_simpledirective()
+{
+
+}
+
+void ConfigParser::state_openblock()
+{
+
+}
+
+void ConfigParser::state_closeblock()
+{
+
+}
+
+
 
 // DEBUG PRINTING:
 
