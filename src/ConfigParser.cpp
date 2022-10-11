4ae07df17de17d0a636847b3a28c1edb884fb6fd
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-10-11 13:29:59 +0200

    Finished basic parsing. Only works for valid files (still need to do error checking for invalid files), and only checks for spaces as argument delimiters, not other whitespace chars. All still in the same file, which I might split up into separate files in their own parsing folder instead. Fixing all the above are still my TODOs.

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,11 +1,13 @@
 #include "ConfigParser.hpp"
 
 #include "utils.hpp"
+#include "stringutils.hpp"
 
 //  GENERAL ORGANISATION:
 
 ConfigParser::ConfigParser() {
 	m_main_context.name = "main";
+	m_main_context.parent_context = NULL;
 	m_tokens[SPACE] = ' ';
 	m_tokens[SEMICOLON] = ';';
 	m_tokens[COMMENT] = '#';
@@ -60,59 +62,95 @@ void ConfigParser::discardComments(std::vector<std::string>& config) {
 
 void ConfigParser::finite_state_machine(std::vector<std::string>& file)
 {
+	t_block_directive *context = &m_main_context;
 	std::vector<std::string>::iterator it;
+	
 	for (it = file.begin(); it != file.end(); ++it) {
+		*it = trimLeadingWhiteSpace(*it);
 		size_t pos = (*it).find_first_of(m_tokens + SEMICOLON, 0, SIZE); // Find first token in line that isn't a space
 		if (pos != std::string::npos){
 			// print((*it)[pos]);
-			// switch ((*it)[pos]):
-			// {
-			// 	case m_tokens[SEMICOLON]:
-			// 	case m_tokens[OPEN_BRACE]:
-			// 	case m_tokens[CLOSE_BRACE]:
-			// 	default:
-			// 		continue //Pointer to current state and run that function?
-			// }
+			// print(context->name); 
+			switch ((*it)[pos])
+			{
+				case (';'):
+					state_simpledirective(&context, it);
+					break;
+				case ('{'):
+					state_openblock(&context, it);
+					break;
+				case ('}'):
+					state_closeblock(&context, it);
+					break;
+				default:
+					continue; //Pointer to current state and run that function?
+			}
 		}
 	}
 }
 
-void ConfigParser::state_simpledirective()
+void ConfigParser::state_simpledirective(t_block_directive **context, std::vector<std::string>::iterator it)
 {
-
+	t_simple_directive tmp;
+	std::string::iterator str_i = (*it).begin();
+	std::string	*field = &(tmp.name);
+	while(*str_i != m_tokens[SEMICOLON])
+	{
+		if(*str_i == m_tokens[SPACE] && field == &(tmp.name))
+			field = &(tmp.params);
+		*field = *field + *str_i;
+		str_i++;
+		if(str_i == (*it).end()){
+			++it;
+			str_i = (*it).begin();
+		}
+	}
+	tmp.params = trimLeadingWhiteSpace(tmp.params);
+	(*context)->simple_directives.push_back(tmp);
+	return;
 }
 
-void ConfigParser::state_openblock()
+void ConfigParser::state_openblock(t_block_directive **context, std::vector<std::string>::iterator it)
 {
-
+	t_block_directive tmp;
+	tmp.parent_context = *context;
+	std::string::iterator str_i;
+	for (str_i = (*it).begin(); str_i != (*it).end() && *str_i != m_tokens[SPACE]; ++str_i)
+		tmp.name = tmp.name + *str_i;
+	(*context)->block_directives.push_back(tmp);
+	(*context) = &((*context)->block_directives.back());
+	++it;
 }
 
-void ConfigParser::state_closeblock()
+void ConfigParser::state_closeblock(t_block_directive **context, std::vector<std::string>::iterator it)
 {
-
+	(*context) = (*context)->parent_context;
+	++it;
 }
 
 
 
 //  DEBUG PRINTING:
 
-void ConfigParser::debug_print_simple(t_simple_directive s) {
-	std::cout << s.name << "   " << s.params << ";" << std::endl;
+void ConfigParser::debug_print_simple(t_simple_directive s, std::string tabs) {
+	std::cout << tabs << "Name: |" << s.name << "| Params: |" << s.params << "|;" << std::endl;
 }
 
-void ConfigParser::debug_print_block(t_block_directive b) {
-	std::cout << b.name << " {" << std::endl;
+void ConfigParser::debug_print_block(t_block_directive b, std::string tabs) {
+	std::cout << tabs << b.name << std::endl << tabs << "{" << std::endl;
+	tabs = tabs + "   ";
 
 	std::vector<t_simple_directive>::iterator it_s;
 	for (it_s = b.simple_directives.begin(); it_s != b.simple_directives.end(); ++it_s)
-		debug_print_simple(*it_s);
+		debug_print_simple(*it_s, tabs);
+	
 	std::vector<t_block_directive>::iterator it_b;
 	for (it_b = b.block_directives.begin(); it_b != b.block_directives.end(); ++it_b)
-		debug_print_block(*it_b);
-	std::cout << std::endl << "}" << std::endl;
+		debug_print_block(*it_b, tabs);
+	std::cout << tabs.substr(3) << "}" << std::endl;
 }
 
 void ConfigParser::debug_print_config() {
 	std::cout << "DEBUG PRINTING CONFIG FILE THAT HAS BEEN PARSED:" << std::endl;
-	debug_print_block(m_main_context);
+	debug_print_block(m_main_context, "");
 }
