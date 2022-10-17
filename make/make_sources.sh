61039cb883d5a2d78bd717a50c14881122e4238f
Job van der Zweep <jobvan-d@student.codam.nl>
2022-10-17 18:44:52 +0200

    fix broken wildcard in make_sources.sh

--- make/make_sources.sh
+++ make/make_sources.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "SOURCES = \\" > make/sources.mk;
-find src -type f -name *.cpp | awk '{print "\t" $0 " \\"}' >> make/sources.mk ;
+find src -type f -name '*.cpp' | awk '{print "\t" $0 " \\"}' >> make/sources.mk ;
 
 echo "HEADERS = \\" > make/headers.mk;
-find include -type f -name *.hpp | awk '{print "\t" $0 " \\"}' >> make/headers.mk ;
+find include -type f -name '*.hpp' | awk '{print "\t" $0 " \\"}' >> make/headers.mk ;
 
