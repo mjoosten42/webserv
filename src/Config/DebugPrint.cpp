8d9ca48bbb4e85b1215fae802fcb7413ad617deb
Maarten Joosten <mjoosten@student.codam.nl>
2022-10-18 11:46:52 +0200

    Actually use tabs

--- src/Config/DebugPrint.cpp
+++ src/Config/DebugPrint.cpp
@@ -6,22 +6,21 @@ void ConfigParser::debug_print() {
 }
 
 void ConfigParser::debug_print_simple(t_simple_directive s, std::string tabs) {
-	std::cout << tabs << s.name << "	" << s.params << ";" << std::endl;
+	std::cout << tabs << s.name << "\t" << s.params << ";" << std::endl;
 }
 
 void ConfigParser::debug_print_block(t_block_directive b, std::string tabs) {
-	if (b.additional_params.empty())
-		std::cout << std::endl << tabs << b.name << "{" << std::endl;
-	else
-		std::cout << std::endl << tabs << b.name << " " << b.additional_params << "{" << std::endl;
-	tabs = tabs + "   ";
+	std::cout << tabs << b.name;
+	if (!b.additional_params.empty())
+		std::cout << " " << b.additional_params;
+	std::cout << " {" << std::endl;
 
 	std::vector<t_simple_directive>::iterator it_s;
 	for (it_s = b.simple_directives.begin(); it_s != b.simple_directives.end(); ++it_s)
-		debug_print_simple(*it_s, tabs);
+		debug_print_simple(*it_s, tabs + "\t");
 
 	std::vector<t_block_directive>::iterator it_b;
 	for (it_b = b.block_directives.begin(); it_b != b.block_directives.end(); ++it_b)
-		debug_print_block(*it_b, tabs);
-	std::cout << tabs.substr(3) << "}" << std::endl;
+		debug_print_block(*it_b, tabs + "\t");
+	std::cout << tabs << "}" << std::endl;
 }
