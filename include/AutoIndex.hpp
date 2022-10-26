654e847902a2137b905b913b4e5f021bfc39a928
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-10-26 15:54:40 +0200

    Wrote a recursivePathCount (to supplement recursiveFileCount) which returns the name of the path relative to the website's root, rather than just the file name.

--- include/AutoIndex.hpp
+++ include/AutoIndex.hpp
@@ -5,3 +5,8 @@
 unsigned int recursiveFileCount(const std::string		  directory,
 								std::vector<std::string>& file_structure,
 								std::string				  tabulation = "");
+
+unsigned int recursivePathCount(const std::string		  directory,
+								std::vector<std::string>& file_structure,
+								std::string				  tabulation = "");
+
--- src/AutoIndex.cpp
+++ src/AutoIndex.cpp
@@ -23,3 +23,32 @@ unsigned int recursiveFileCount(const std::string directory, std::vector<std::st
 	closedir(derp);
 	return (ret);
 }
+
+// Same as the above, except it saves a vector of paths relative to the site's root.
+unsigned int recursivePathCount(const std::string directory, std::vector<std::string>& file_structure, std::string tabulation) {
+	DIR			*derp; // DirEctoRy Pointer
+	unsigned int ret = 0;
+
+	derp = opendir(directory.c_str());
+	if (derp == NULL)
+		return (ret);
+	struct dirent *contents;
+	// tabulation = tabulation + directory + "/";
+	while ((contents = readdir(derp)) != NULL) {
+		std::string name = contents->d_name;
+		if (name != "." && name != "..") {
+			file_structure.push_back(tabulation + name);
+			ret++;
+			std::string next_dir = directory + "/" + name;
+			ret					 = ret + recursiveFileCount(next_dir, file_structure, tabulation + name + "/");
+		}
+	}
+	closedir(derp);
+	return (ret);
+}
+
+// void	autoIndexHtml()
+// {
+	
+// 	return ;
+// }
