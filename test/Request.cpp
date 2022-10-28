68a6bdba9b7cd6a21814717e68ce63dcf0718c51
Job van der Zweep <jobvan-d@student.codam.nl>
2022-10-28 15:41:40 +0200

    Fix test

--- test/Request.cpp
+++ test/Request.cpp
@@ -2,15 +2,15 @@
 
 #include "Request.hpp"
 
-static int easySend(Request& req, const std::string& raw) {
+static void easySend(Request& req, const std::string& raw) {
 
-	return req.append(raw.c_str(), raw.length());
+	req.append(raw.c_str(), raw.length());
 }
 
 TEST_CASE( "Request generic", "[Request]") {
 
 	Request r;
-	REQUIRE( easySend(r, "GET / HTTP/1.1\r\ndERP:     cool\r\nHOST: yeet\r\n\r\n") == 200 );
+	easySend(r, "GET / HTTP/1.1\r\ndERP:     cool\r\nHOST: yeet\r\n\r\n");
 	REQUIRE( r.getMethod() == GET );
 	REQUIRE( r.getLocation() == "/" );
 	REQUIRE( r.getHeaderValue("derp") == "cool" );
