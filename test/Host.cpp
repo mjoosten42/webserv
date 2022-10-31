8eeff1d18f8eb8a6e36fafd6b266575e6a4bb006
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-10-31 14:55:44 +0100

    Rewrote tests to match the new test.conf file.

--- test/Host.cpp
+++ test/Host.cpp
@@ -18,23 +18,23 @@ TEST_CASE( "Hosts", "[Server]" ) {
 	std::string test;
 
 	loc_index = s.getLocationIndexForFile("amogus.jpg");
-	REQUIRE( loc_index == 0);
+	REQUIRE( loc_index == 1);
 	test = s.getRootForFile(loc_index, "amogus.jpg");
-	REQUIRE( test == "html/img"); //Check loc 0 for file it contains
+	REQUIRE( test == "html/img"); //Check loc 1 for file it contains
 
 	loc_index = s.getLocationIndexForFile("amogus_bogus_file");
-	REQUIRE( loc_index == 1);
+	REQUIRE( loc_index == 2);
 	test = s.getRootForFile(loc_index, "amogus_bogus_file");
-	REQUIRE( test == "html/second_dir"); //Check loc 1 for file it contains
+	REQUIRE( test == "html/second_dir"); //Check loc 2 for file it contains
 
-	test = s.getRootForFile(0, "amogus_bogus_file");
-	REQUIRE( test == "");  //Check loc 0 for file in loc 1
+	test = s.getRootForFile(1, "amogus_bogus_file");
+	REQUIRE( test == "");  //Check loc 1 for file in loc 2
 
 	test = s.getRootForFile(-1, "amogus.jpg");
 	REQUIRE( test == "html/img"); //Check whole server for file in loc 0
 
 	loc_index = s.getLocationIndexForFile("404.html");
-	REQUIRE( loc_index == -1);
+	REQUIRE( loc_index == 0);
 	test = s.getRootForFile(loc_index, "404.html");
 	REQUIRE( test == "html"); // Check for file in server root
 }
