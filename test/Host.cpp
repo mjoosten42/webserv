26ad2c7b0669e1d8294f32133f51cc16d711898a
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-10-31 14:51:51 +0100

    Added some files for testing. Though the funcion I wrote works, Job pointed out we need to do something else.. We don't need to find the path to any given random file on our server, but rather translate the user specified server 'Address' to a client-side file 'Path'.

--- test/Host.cpp
+++ test/Host.cpp
@@ -8,12 +8,33 @@
 // TODO: fix this test
 TEST_CASE( "Hosts", "[Server]" ) {
 	ConfigParser config;
-	config.parse_config("default.conf");
+	config.parse_config("test.conf");
 
 	std::vector<Listener> listeners;
 	initFromConfig(config, listeners);
 
-	std::string test = listeners.front().getServerByHost("amogus.localhst.co.uk").getRootForFile(0, "amogus.jpg");
+	Server s = listeners.front().getServerByHost("amogus.localhst.co.uk");
+	int loc_index;
+	std::string test;
 
-	REQUIRE( test == "html/img");
+	loc_index = s.getLocationIndexForFile("amogus.jpg");
+	REQUIRE( loc_index == 0);
+	test = s.getRootForFile(loc_index, "amogus.jpg");
+	REQUIRE( test == "html/img"); //Check loc 0 for file it contains
+
+	loc_index = s.getLocationIndexForFile("amogus_bogus_file");
+	REQUIRE( loc_index == 1);
+	test = s.getRootForFile(loc_index, "amogus_bogus_file");
+	REQUIRE( test == "html/second_dir"); //Check loc 1 for file it contains
+
+	test = s.getRootForFile(0, "amogus_bogus_file");
+	REQUIRE( test == "");  //Check loc 0 for file in loc 1
+
+	test = s.getRootForFile(-1, "amogus.jpg");
+	REQUIRE( test == "html/img"); //Check whole server for file in loc 0
+
+	loc_index = s.getLocationIndexForFile("404.html");
+	REQUIRE( loc_index == -1);
+	test = s.getRootForFile(loc_index, "404.html");
+	REQUIRE( test == "html"); // Check for file in server root
 }
