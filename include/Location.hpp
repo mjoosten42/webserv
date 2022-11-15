56ce2f09b4dfdbb7fcf95102d668d11f948568f4
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-11-15 17:24:25 +0100

    Applied formatting, added limit_except parsing from config to Location blocks. Still need to actually limit the disallowed methods.

--- include/Location.hpp
+++ include/Location.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ConfigParser.hpp"
+#include "Request.hpp"
 
 #include <iostream>
 #include <map>
@@ -15,13 +16,16 @@ class Location {
 
 		std::string m_location;
 
-		std::string m_root;
-		std::string m_indexPage;
-		std::string m_limit_except; // IE only allow GET, POST
-		std::string m_redirect;		// redirect /cgi-bin/
+		std::string			 m_root;
+		std::string			 m_indexPage;
+		std::vector<methods> m_limit_except; // IE only allow GET, POST
+		std::string			 m_redirect;	 // redirect /cgi-bin/
 
 		std::vector<std::string> m_CGIs;
 
 		int	 m_client_max_body_size;
 		bool m_auto_index;
+
+	private:
+		void getLimitExceptFromConfig(t_block_directive *constructor_specs);
 };
--- src/Location.cpp
+++ src/Location.cpp
@@ -1,8 +1,35 @@
 #include "Location.hpp"
 
 #include "Server.hpp"
+#include "logger.hpp"
 #include "stringutils.hpp"
 #include "utils.hpp"
+
+void Location::getLimitExceptFromConfig(t_block_directive *constructor_specs) {
+	std::string				 val_from_config;
+	std::vector<std::string> methods_as_strings = { "GET", "POST", "DELETE" };
+
+	val_from_config = constructor_specs->fetch_simple("limit_except");
+	if (!val_from_config.empty())
+		methods_as_strings = stringSplit(val_from_config);
+
+	// if (std::find(methods_as_strings.begin(), methods_as_strings.end(), "GET") != methods_as_strings.end())
+	// 	m_limit_except.push_back(GET);
+	// if (std::find(methods_as_strings.begin(), methods_as_strings.end(), "POST") != methods_as_strings.end())
+	// 	m_limit_except.push_back(POST);
+	// if (std::find(methods_as_strings.begin(), methods_as_strings.end(), "DELETE") != methods_as_strings.end())
+	// 	m_limit_except.push_back(DELETE);
+
+	for (auto method : methods_as_strings) { // included the non-auto method above should we need to revert back to it.
+		if (method == "GET")
+			m_limit_except.push_back(GET);
+		if (method == "POST")
+			m_limit_except.push_back(POST);
+		if (method == "DELETE")
+			m_limit_except.push_back(DELETE);
+	}
+	return;
+}
 
 Location::Location(): m_location("/"), m_root("html"), m_indexPage("index.html"), m_client_max_body_size(0) {}
 
@@ -25,9 +52,9 @@ Location::Location(t_block_directive *constructor_specs, Server *parent):
 	if (!val_from_config.empty())
 		m_client_max_body_size = stringToIntegral<int>(val_from_config);
 
-	val_from_config = constructor_specs->fetch_simple("limit_except");
-	if (!val_from_config.empty())
-		m_limit_except = val_from_config;
+	getLimitExceptFromConfig(constructor_specs);
+	for (auto except : m_limit_except)
+		LOG(except);
 
 	val_from_config = constructor_specs->fetch_simple("autoindex");
 	if (!val_from_config.empty())
