8e45ebba99ffbd82826726570aea1253edb9b0a2
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-11-24 16:52:54 +0100

    Added tests for missing semicolons, restructured the tester file.

--- test/Config.cpp
+++ test/Config.cpp
@@ -15,6 +15,48 @@ unsigned int    try_invalid_config(std::string path)
     return (caught_exceptions);
 }
 
+void    test_all_invalid(std::string invalid_dir)
+{
+    // Numeric vals - negatives
+    // Negative port
+    REQUIRE(try_invalid_config(invalid_dir + "invalid1.conf") == 1);
+    // Negative CMB in server
+    REQUIRE(try_invalid_config(invalid_dir + "invalid2.conf") == 1);
+    // Negative CMB in location
+    REQUIRE(try_invalid_config(invalid_dir + "invalid3.conf") == 1);
+    // Negative Error Page in server
+    REQUIRE(try_invalid_config(invalid_dir + "invalid4.conf") == 1);
+    // Negative Error Page in location
+    REQUIRE(try_invalid_config(invalid_dir + "invalid5.conf") == 1);
+
+    // Numeric vals - overflows
+    // Port larger than max ushort
+    REQUIRE(try_invalid_config(invalid_dir + "invalid6.conf") == 1);
+    // CMB larger than max size_t in server
+    REQUIRE(try_invalid_config(invalid_dir + "invalid7.conf") == 1);
+    // CMB larger than max size_t in location
+    REQUIRE(try_invalid_config(invalid_dir + "invalid8.conf") == 1);
+    // Error page larger than max uint in location
+    REQUIRE(try_invalid_config(invalid_dir + "invalid9.conf") == 1);
+    // Error page larger than max uint in server
+    REQUIRE(try_invalid_config(invalid_dir + "invalid10.conf") == 1);
+
+    //Invalid brace-pairing
+    REQUIRE(try_invalid_config(invalid_dir + "invalid11.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid12.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid13.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid14.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid15.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid16.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid17.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid18.conf") == 1);
+
+    //Missing semicolons
+    REQUIRE(try_invalid_config(invalid_dir + "invalid19.conf") == 1);
+    REQUIRE(try_invalid_config(invalid_dir + "invalid20.conf") == 1);
+    return;
+}
+
 TEST_CASE( "Configs", "[Config]")
 {
     std::string config_root_dir = "./test/Config/conf_files/";
@@ -27,39 +69,8 @@ TEST_CASE( "Configs", "[Config]")
     // Port max ushort 65535
     // Error page max uint 4294967295
     // CMB max size_t 18446744073709551615
+    // Multi-line simple directive
 
+    test_all_invalid(config_root_dir + "invalid_configs/");
 
-    // Numeric vals - negatives
-    // Negative port
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid1.conf") == 1);
-    // Negative CMB in server
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid2.conf") == 1);
-    // Negative CMB in location
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid3.conf") == 1);
-    // Negative Error Page in server
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid4.conf") == 1);
-    // Negative Error Page in location
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid5.conf") == 1);
-
-    // Numeric vals - overflows
-    // Port larger than max ushort
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid6.conf") == 1);
-    // CMB larger than max size_t in server
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid7.conf") == 1);
-    // CMB larger than max size_t in location
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid8.conf") == 1);
-    // Error page larger than max uint in location
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid9.conf") == 1);
-    // Error page larger than max uint in server
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid10.conf") == 1);
-
-    //Invalid brace-pairing
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid11.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid12.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid13.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid14.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid15.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid16.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid17.conf") == 1);
-    REQUIRE(try_invalid_config(config_root_dir + "invalid_configs/invalid18.conf") == 1);
 }
